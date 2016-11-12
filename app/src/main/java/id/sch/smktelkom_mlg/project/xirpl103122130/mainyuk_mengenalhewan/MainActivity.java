95be87177fd930def05c12d92f2b34063eb0f018
Faiqoh <faiqoh11.fa@gmail.com>
2016-11-11 20:59:33 -0800

    - tebak gambar v1

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl103122130/mainyuk_mengenalhewan/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl103122130/mainyuk_mengenalhewan/MainActivity.java
@@ -1,7 +1,10 @@
 package id.sch.smktelkom_mlg.project.xirpl103122130.mainyuk_mengenalhewan;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        findViewById(R.id.imageButtonTGambar).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, TebakgambarActivity.class));
+            }
+        });
+
+        findViewById(R.id.imageButtonTSuara).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, TebaksuaraActivity.class));
+            }
+        });
+
+        ImageButton exit = (ImageButton) findViewById(R.id.imageButtonExit);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
+            }
+        });
 
     }
 }
