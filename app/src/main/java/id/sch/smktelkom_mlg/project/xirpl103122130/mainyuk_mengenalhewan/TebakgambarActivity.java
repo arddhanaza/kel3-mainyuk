a68fd54dd6021d844b34abf63d38fe77e666ba84
Nugrahanto <rianirsyahnugrahanto@gmail.com>
2016-11-14 18:12:33 +0700

    Gambar example

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl103122130/mainyuk_mengenalhewan/TebakgambarActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl103122130/mainyuk_mengenalhewan/TebakgambarActivity.java
@@ -1,54 +1,30 @@
 package id.sch.smktelkom_mlg.project.xirpl103122130.mainyuk_mengenalhewan;
 
-import android.content.res.TypedArray;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.GridView;
-
-import java.util.ArrayList;
+import android.view.View;
 
 public class TebakgambarActivity extends AppCompatActivity {
-
-      private GridView gridView;
-      private GridViewAdapter gridAdapter;
 
       @Override
       protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_tebakgambar);
 
-            gridView = (GridView) findViewById(R.id.gridView);
-            gridAdapter = new GridViewAdapter(this, R.layout.grid_item_layout, getData());
-            gridView.setAdapter(gridAdapter);
-
-            /*gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                  public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-                        ImageItem item = (ImageItem) parent.getItemAtPosition(position);
-
-                      startActivity(new Intent());
-                  }
-            });*/
-            /*findViewById(R.id.imageBt).setOnClickListener(new View.OnClickListener() {
+            findViewById(R.id.satu).setOnClickListener(new View.OnClickListener() {
                   @Override
                   public void onClick(View v) {
-                        startActivity(new Intent(TebakgambarActivity.this, gambarActivity.class));
+                        startActivity(new Intent(TebakgambarActivity.this, gambarSatu.class));
                   }
-            });*/
+            });
 
-      }
+            findViewById(R.id.dua).setOnClickListener(new View.OnClickListener() {
+                  @Override
+                  public void onClick(View v) {
+                        startActivity(new Intent(TebakgambarActivity.this, gambarDua.class));
+                  }
+            });
 
-      /**
-       * Prepare some dummy data for gridview
-       */
-      private ArrayList<ImageItem> getData() {
-            final ArrayList<ImageItem> imageItems = new ArrayList<>();
-            TypedArray imgs = getResources().obtainTypedArray(R.array.image_ids);
-            for (int i = 0; i < imgs.length(); i++) {
-                  Bitmap bitmap = BitmapFactory.decodeResource(getResources(), imgs.getResourceId(i, -1));
-                  imageItems.add(new ImageItem(bitmap, "Gambar Ke " + i));
-            }
-            return imageItems;
       }
 }
---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl103122130/mainyuk_mengenalhewan/gambarSatu.java
@@ -0,0 +1,35 @@
+package id.sch.smktelkom_mlg.project.xirpl103122130.mainyuk_mengenalhewan;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class gambarSatu extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_gambar_satu);
+
+		findViewById(R.id.iButtonPilihanA).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+			}
+		});
+
+		findViewById(R.id.iButtonPilihanB).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+			}
+		});
+
+		findViewById(R.id.iButtonPilihanC).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+			}
+		});
+	}
+}
