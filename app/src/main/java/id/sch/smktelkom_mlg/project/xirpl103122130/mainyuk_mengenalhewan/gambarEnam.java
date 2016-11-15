c61b24b8f2a9d17a4debcc8c98fc98d9dc7bcc86
Faiqoh <faiqoh11.fa@gmail.com>
2016-11-15 14:40:20 -0800

    Validasi gambar finishing

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl103122130/mainyuk_mengenalhewan/gambarEnam.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl103122130/mainyuk_mengenalhewan/gambarEnam.java
@@ -1,7 +1,12 @@
 package id.sch.smktelkom_mlg.project.xirpl103122130.mainyuk_mengenalhewan;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class gambarEnam extends AppCompatActivity {
 
@@ -9,5 +14,43 @@ public class gambarEnam extends AppCompatActivity {
       protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_gambar_enam);
+
+            findViewById(R.id.iButtonPilihanA).setOnClickListener(new View.OnClickListener() {
+                  @Override
+                  public void onClick(View v) {
+                        AlertDialog.Builder AlertBox = new AlertDialog.Builder(gambarEnam.this, android.R.style.Theme_Holo_Light_Dialog_NoActionBar_MinWidth);
+                        AlertBox.setTitle("Opss!");
+                        AlertBox.setMessage("Gambar yang anda pilih salah");
+                        AlertBox.setNeutralButton("OK", null);
+                        AlertBox.show();
+                  }
+            });
+
+            findViewById(R.id.iButtonPilihanB).setOnClickListener(new View.OnClickListener() {
+                  @Override
+                  public void onClick(View v) {
+                        AlertDialog.Builder AlertBox = new AlertDialog.Builder(gambarEnam.this, android.R.style.Theme_Holo_Light_Dialog_NoActionBar_MinWidth);
+                        AlertBox.setTitle("Opss!");
+                        AlertBox.setMessage("Gambar yang anda pilih salah");
+                        AlertBox.setNeutralButton("OK", null);
+                        AlertBox.show();
+                  }
+            });
+
+            findViewById(R.id.iButtonPilihanC).setOnClickListener(new View.OnClickListener() {
+                  @Override
+                  public void onClick(View v) {
+                        AlertDialog.Builder AlertBox = new AlertDialog.Builder(gambarEnam.this, android.R.style.Theme_Holo_Light_Dialog_NoActionBar_MinWidth);
+                        AlertBox.setTitle("Yeeii Benar!");
+                        AlertBox.setMessage("Buka gambar yang lain");
+                        AlertBox.setNeutralButton("OK", new DialogInterface.OnClickListener() {
+                              public void onClick(DialogInterface dialog, int id) {
+
+                                    startActivity(new Intent(((Dialog) dialog).getContext(), TebakgambarActivity.class));
+                              }
+                        });
+                        AlertBox.show();
+                  }
+            });
       }
 }
--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl103122130/mainyuk_mengenalhewan/gambarTiga.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl103122130/mainyuk_mengenalhewan/gambarTiga.java
@@ -1,6 +1,10 @@
 package id.sch.smktelkom_mlg.project.xirpl103122130.mainyuk_mengenalhewan;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -14,21 +18,38 @@ public class gambarTiga extends AppCompatActivity {
             findViewById(R.id.iButtonPilihanA).setOnClickListener(new View.OnClickListener() {
                   @Override
                   public void onClick(View v) {
+                        AlertDialog.Builder AlertBox = new AlertDialog.Builder(gambarTiga.this, android.R.style.Theme_Holo_Light_Dialog_NoActionBar_MinWidth);
+                        AlertBox.setTitle("Yeeii Benar!");
+                        AlertBox.setMessage("Buka gambar yang lain");
+                        AlertBox.setNeutralButton("OK", new DialogInterface.OnClickListener() {
+                              public void onClick(DialogInterface dialog, int id) {
 
+                                    startActivity(new Intent(((Dialog) dialog).getContext(), TebakgambarActivity.class));
+                              }
+                        });
+                        AlertBox.show();
                   }
             });
 
             findViewById(R.id.iButtonPilihanB).setOnClickListener(new View.OnClickListener() {
                   @Override
                   public void onClick(View v) {
-
+                        AlertDialog.Builder AlertBox = new AlertDialog.Builder(gambarTiga.this, android.R.style.Theme_Holo_Light_Dialog_NoActionBar_MinWidth);
+                        AlertBox.setTitle("Opss!");
+                        AlertBox.setMessage("Gambar yang anda pilih salah");
+                        AlertBox.setNeutralButton("OK", null);
+                        AlertBox.show();
                   }
             });
 
             findViewById(R.id.iButtonPilihanC).setOnClickListener(new View.OnClickListener() {
                   @Override
                   public void onClick(View v) {
-
+                        AlertDialog.Builder AlertBox = new AlertDialog.Builder(gambarTiga.this, android.R.style.Theme_Holo_Light_Dialog_NoActionBar_MinWidth);
+                        AlertBox.setTitle("Opss!");
+                        AlertBox.setMessage("Gambar yang anda pilih salah");
+                        AlertBox.setNeutralButton("OK", null);
+                        AlertBox.show();
                   }
             });
       }
